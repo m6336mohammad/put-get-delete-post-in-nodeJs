fefa588317420828a5c2f37f086e751bc8f3e05e
mohammad <m6336.programer@gmail.com>
2023-10-07 15:51:13 +0330

    addes get function by params '/:id'

--- app.js
+++ app.js
@@ -6,14 +6,17 @@ const port = process.env.PORT || 3000; // if not font port number on the .env fi
 
 
 
-
 //added get function for root get '/'
 app.get('/', (req, res)=>{
   console.log(req.body);
   res.send("welcom to home page")
 })
 
-
+//added get function for get by id
+app.get('/api/user/:id', (req, res)=>{
+  console.log(req.params.id);
+  res.send("welcom to home page")
+})
 
 
 
