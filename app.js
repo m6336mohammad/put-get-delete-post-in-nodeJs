64c4e1ae6e7cedf10d3b6d993b17cc6b7f132e97
mohammad <m6336.programer@gmail.com>
2023-10-07 15:44:55 +0330

    addes get function for root get

--- app.js
+++ app.js
@@ -4,6 +4,17 @@ const app =express();
 
 const port = process.env.PORT || 3000; // if not font port number on the .env file assign default port 3000
 
+//parse for req.body 
+app.use(express.json())
+
+
+//added get function for root get '/'
+app.get('/', (req, res)=>{
+  console.log(req.body);
+  res.send("welcom to home page")
+})
+
+
 
 app.listen(port, ()=>{
   console.log(`server run on port: ${port}`);
