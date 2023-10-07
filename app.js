5f3922ed347e0fa4dbf64ac55d8c6562cc380ff3
mohammad <m6336.programer@gmail.com>
2023-10-07 15:48:16 +0330

    added get functoin for root '/' request by browser or post man

--- app.js
+++ app.js
@@ -4,8 +4,7 @@ const app =express();
 
 const port = process.env.PORT || 3000; // if not font port number on the .env file assign default port 3000
 
-//parse for req.body 
-app.use(express.json())
+
 
 
 //added get function for root get '/'
@@ -13,6 +12,8 @@ app.get('/', (req, res)=>{
   console.log(req.body);
   res.send("welcom to home page")
 })
+
+
 
 
 
