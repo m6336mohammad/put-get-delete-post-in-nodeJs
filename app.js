2ba73edb056bd47d8f2572422c3c3a03a4c192ca
mohammad <m6336.programer@gmail.com>
2023-10-07 15:15:49 +0330

    import dotenv file and server listen

--- app.js
+++ app.js
@@ -1,1 +1,10 @@
-import express from 'express'
+import 'dotenv/config';
+import express from 'express';
+const app =express();
+
+const port = process.env.PORT || 3000; // if not font port number on the .env file assign default port 3000
+
+
+app.listen(port, ()=>{
+  console.log(`server run on port: ${port}`);
+})
