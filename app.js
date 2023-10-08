fe542313215da6756ab2fa47ebce98697cabda51
minakarimi1 <minakarimi1@yahoo.com>
2023-10-08 18:55:35 +0330

    added status

--- app.js
+++ app.js
@@ -12,16 +12,15 @@ const port = process.env.PORT || 3000; // if not font port number on the .env fi
 app.get('/', (req, res)=>{
   console.log(req.body);
   // res.send("welcom to home page" )
-  res.json(country)
+  res.status(200).json(country)
 })
 
 //added get function for get by id '/:id'
 app.get('/api/user/:id', (req, res)=>{
   const cont = country.find((c)=> c.id === parseInt(req.params.id));
-  if(!cont){return res.send('this counmter not find');}
-  res.send({data:cont , massege: 'ok'});
+  if(!cont){return res.status(400).send('this counmter not find');}
+  res.status(200).send({data:cont , massege: 'ok'});
   console.log(req.params.id);
-  // res.send('your request ID ==>: ' +req.params.id )
 })
 
 //added post
