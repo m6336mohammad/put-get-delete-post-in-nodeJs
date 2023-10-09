7e9dbec1532eb27b5076834e52500c67a45c4629
mina <43753017+minakarimi1@users.noreply.github.com>
2023-10-09 15:20:30 +0330

    Dev2 post validation (#5)
    * added validation in country name
    
    * addead validation in packege

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 import "dotenv/config";
 import express from "express";
 import country from "./country.js";
+import { body, validationResult } from 'express-validator';
 
 const app = express();
 
@@ -17,7 +18,7 @@ app.get("/", (req, res) => {
 });
 
 //added get function for get by id '/:id'
-app.get("/api/user/:id", (req, res) => {
+app.get("/api/country/:id", (req, res) => {
   const cont = country.find((c) => c.id === parseInt(req.params.id));
   if (!cont) {
     return res.status(400).send("this counmter not find");
@@ -27,9 +28,12 @@ app.get("/api/user/:id", (req, res) => {
 });
 
 //added post
-app.post("/api/country", (req, res) => {
+app.post("/api/country",[
+  body('name','country name can not be empty').notEmpty(),
+], (req, res) => {
+  const error = validationResult(req);
+  if(!error.isEmpty()){return res.status(400).json({data: null ,error: error.array() , massege: 'validation error: you shold be enter country name'})}
   country.push({ id: country.length + 1, ...req.body });
-  // console.log(req.body);
   res.status(200).json({
     data: country,
     message: "ok",
