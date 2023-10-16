47699c371d0de2abfa080320f65e02aed897e7cc
mohammad <m6336.programer@gmail.com>
2023-10-16 20:03:48 +0330

    added validation on top

--- app.js
+++ app.js
@@ -9,22 +9,22 @@ const port = process.env.PORT || 3000; // if not font port number on the .env fi
 app.use(express.json());
 
 //>>>> added get <<<<
-  //added get function for get '/'
+//added get function for get '/'
 app.get("/", (req, res) => {
   console.log(req.body);
-  res.send("welcom to home page" )
+  res.send("welcom to home page");
   // res.status(200).json(country);
 });
 
 //>>>> added get <<<<
-  //added get function for get '/api/country'
+//added get function for get '/api/country'
 app.get("/api/country", (req, res) => {
   console.log(req.body);
   // res.send("welcom to home page" )
   res.status(200).json(country);
 });
 
-  //added get function for get by id '/:id'
+//added get function for get by id '/:id'
 app.get("/api/country/:id", (req, res) => {
   const cont = country.find((c) => c.id === parseInt(req.params.id));
   if (!cont) {
@@ -41,16 +41,13 @@ app.post(
   (req, res) => {
     const error = validationResult(req);
     if (!error.isEmpty()) {
-      return res
-        .status(400)
-        .json({
-          data: null,
-          error: error.array(),
-          massege: "validation error: you shold be enter country name",
-        });
+      return res.status(400).json({
+        data: null,
+        error: error.array(),
+        massege: "validation error: you shold be enter country name",
+      });
     }
 
-    
     country.push({ id: country.length + 1, ...req.body });
     res.status(200).json({
       data: country,
@@ -65,25 +62,22 @@ app.put(
   "/api/country/:id",
   [body("name", "country name can not be empty").notEmpty()],
   (req, res) => {
-    
+    // validate
+    const error = validationResult(req);
+    if (!error.isEmpty()) {
+      return res.status(400).json({
+        data: null,
+        error: error.array(),
+        massege: "validation error",
+      });
+    }
+
     //check user
     const countryId = country.find((c) => c.id === parseInt(req.params.id));
     if (!countryId) {
       return res
         .status(400)
         .json({ data: null, massege: "this country id not find" });
-    }
-
-    // validate
-    const error = validationResult(req);
-    if (!error.isEmpty()) {
-      return res
-        .status(400)
-        .json({
-          data: null,
-          error: error.array(),
-          massege: "validation error",
-        });
     }
 
     //map key
@@ -98,21 +92,18 @@ app.put(
   }
 );
 
-
-
 //>>>> added delete <<<<
-app.delete('/api/country/:id' , (req,res)=>{
+app.delete("/api/country/:id", (req, res) => {
+  const countryId = country.find((c) => c.id === parseInt(req.params.id));
+  if (!countryId) {
+    return res.status(400).json({ data: null, massege: "country id not find" });
+  }
 
-const countryId = country.find((c)=> c.id === parseInt(req.params.id));
-if(!countryId){return res.status(400).json({data: null , massege: 'country id not find'})};
+  const index = country.indexOf(countryId);
+  country.splice(index, 1);
 
-const index = country.indexOf(countryId);
-country.splice(index,1);
-
-res.status(200).jsonp({data:country , message: 'ok'})
-})
-
-
+  res.status(200).jsonp({ data: country, message: "ok" });
+});
 
 app.listen(port, () => {
   console.log(`server run on port: ${port}`);
